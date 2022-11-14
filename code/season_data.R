8472417cd9f45e631b7c59b4e0f3e05138e0ac91
jnt0009 <31008913+jnt0009@users.noreply.github.com>
2022-11-14 15:25:42 -0500

    INIT

---
+++ code/season_data.R
@@ -0,0 +1,16 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  "readr",
+  "dplyr",
+  "tidyverse",
+  "rvest"
+)
+
+content <- xml2::read_html("https://www.basketball-reference.com/leagues/NBA_2023_totals.html")
+tables <- content %>% html_table(fill = TRUE)
+players <- tables[[1]]
+players <- players %>% 
+  filter(Rk != "Rk")
+
+
+players %>% write.csv("./data/player_stats.csv")
